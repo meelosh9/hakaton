5a697a93f9821f202157edce3c74fbd3aefa8e96
meelosh9 <91650382+meelosh9@users.noreply.github.com>
2021-11-04 16:36:56 +0100

    a

--- server.js
+++ server.js
@@ -13,7 +13,9 @@ app.use(express.urlencoded({extended : true}))
 
 app.use('/api/user',users)
 
-
+app.use('/',(req, res) => {
+    res.send('Hello World!')
+})
 
 app.listen(5000,() => {
     console.log('Server is listening on port 5000....')
